9a48d0479183ab1e109b7826c98c8fabd2712c5c
Piotr Czapor <p.czapor@ideo.local>
2023-10-20 15:51:17 +0200

    register form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Login from "../src/components/Login";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import { StorageKeys } from "./types";
+import Register, { Step1, Step2, Step3 } from "components/Register";
 
 const App: React.FC<{}> = () => {
   return (
@@ -11,6 +11,11 @@ const App: React.FC<{}> = () => {
       <Route path="/" index element={<Login />}></Route>
 
       <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/register/1" element={<Register/>}>
+        <Route path="" element={<Step1 />}/>
+        <Route path="2" element={<Step2 />}/>
+        <Route path="3" element={<Step3 />}/>
+      </Route>
     </Routes>
   );
 };
